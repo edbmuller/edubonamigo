f315bc05f4da55f32dea0395355ee47ef77aeb5f
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-01 19:14:51 -0300

    REMOVED Unused code from scrollbar.js

--- src/js/animations/scrollbar.js
+++ src/js/animations/scrollbar.js
@@ -133,35 +133,6 @@ const spinScrollSVG = () => {
 	})
 }
 
-/* ADD SKEW SECTION */
-const skewContent = () => {
-	let proxy = { skew: 0 }
-	let skewSetter = gsap.quickSetter('.hide-show', 'skewY', 'deg') // fast
-	let clamp = gsap.utils.clamp(-20, 20) // don't let the skew go beyond 20 degrees.
-
-	ScrollTrigger.create({
-		scroller: '#viewport',
-		trigger: '.root',
-
-		onUpdate: (self) => {
-			let skew = clamp(self.getVelocity() / -300)
-			// only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
-			if (Math.abs(skew) > Math.abs(proxy.skew)) {
-				proxy.skew = skew
-				gsap.to(proxy, {
-					skew: 0,
-					duration: 0.4,
-					ease: 'power3',
-					overwrite: true,
-					onUpdate: () => skewSetter(proxy.skew)
-				})
-			}
-		}
-		// make the right edge "stick" to the scroll bar. force3D: true improves performance
-		// gsap.set(".skewElem", {transformOrigin: "right center", force3D: true});
-	})
-}
-
 const fixGsapMarkers = () => {
 	if (document.querySelector('.gsap-marker-scroller-start')) {
 		const markers = gsap.utils.toArray('[class *= "gsap-marker"]')
