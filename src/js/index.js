519e9bb7de9c285b82ab30e9c24c2f741414db18
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-03 23:43:09 -0300

    ADD build

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../styles/loading.scss'
-import '../styles/index.scss'
-
-// TODO: ativar algum experimento com webgl quando usar o macete
-console.log('Macete: → ↑ ← ↓ e d !')
-
-// TODO: Trigger after imagesLoaded
-import './animations/hero'
-import './animations/magneticElement'
