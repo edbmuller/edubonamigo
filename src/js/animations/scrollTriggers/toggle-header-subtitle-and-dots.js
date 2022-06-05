284418e9c9c9277816c4cdbb93dbefa55d67d35b
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-05 16:05:02 -0300

    REFACTOR variable name and toogle trigger

--- src/js/animations/scrollTriggers/toggle-header-subtitle-and-dots.js
+++ src/js/animations/scrollTriggers/toggle-header-subtitle-and-dots.js
@@ -13,20 +13,20 @@ export default function toggleHeaderSubtitleAndDots() {
 		ease: 'power4.out'
 	}
 
-	DOM.headerSubtitles.forEach((element, index) => {
+	DOM.headerSubtitles.forEach((subtitle, index) => {
 		ScrollTrigger.create({
-			trigger: DOM.sections[index + 1],
-			start: 'top 40%',
-			end: 'bottom 40%',
 			// markers: true,
+			trigger: DOM.sections[index + 1],
+			start: 'top 25%',
+			end: 'bottom 25%',
 			onEnter: () => {
-				gsap.to(element, { y: '-110%', ...configs })
+				gsap.to(subtitle, { y: '-110%', ...configs })
 				DOM.headerDots[index].classList.add('theme--background')
 			},
-			onLeave: () => gsap.to(element, { y: '-220%', ...configs }),
-			onEnterBack: () => gsap.to(element, { y: '-110%', ...configs }),
+			onLeave: () => gsap.to(subtitle, { y: '-220%', ...configs }),
+			onEnterBack: () => gsap.to(subtitle, { y: '-110%', ...configs }),
 			onLeaveBack: () => {
-				gsap.to(element, { y: '0%', ...configs })
+				gsap.to(subtitle, { y: '0%', ...configs })
 				DOM.headerDots[index].classList.remove('theme--background')
 			}
 		})
