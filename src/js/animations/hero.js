8aed92a5cf8bd722dc2f9fd18128a68bb87f3d60
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-05 21:36:45 -0300

    REFACTOR scrollTriggers folder

--- src/js/animations/hero.js
+++ src/js/animations/hero.js
@@ -1,6 +1,6 @@
 import gsap from 'gsap'
 import { Flip } from 'gsap/Flip'
-import initScrollTriggers from './scrollTriggers'
+import initScrollTriggers from '../scrollTriggers'
 
 gsap.registerPlugin(Flip)
 
