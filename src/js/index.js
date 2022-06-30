1f8f6f7fc2b60b79fb54b3ecfca0730861769e8b
Eduardo Bonamigo <edbmuller@gmail.com>
2022-06-30 12:31:18 -0300

    STYLES includes e links

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,9 @@
-import '../styles/loading.scss'
-import '../styles/index.scss'
+// TODO: Trigger after imagesLoaded
+import './animations/toggle-theme'
+// import "./animations/custom-cursor"
+import './animations/hero'
+import './animations/projects'
+import './animations/magneticElement'
 
 // TODO: ativar algum experimento com webgl quando usar o macete
 console.log('Macete: → ↑ ← ↓ e d !')
-
-// TODO: Trigger after imagesLoaded
-import './animations/toggle-theme'
-// import './animations/custom-cursor'
-import './animations/hero'
-import './animations/projects'
-// import './animations/magneticElement'
