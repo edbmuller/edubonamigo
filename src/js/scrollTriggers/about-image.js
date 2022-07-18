4b977fe9d032e459810c788e322ece02d276dfa2
Eduardo Bonamigo <edbmuller@gmail.com>
2022-07-18 15:40:08 -0300

    STYLES about image

--- src/js/scrollTriggers/about-image.js
+++ src/js/scrollTriggers/about-image.js
@@ -16,7 +16,7 @@ export default function showAboutImage() {
 
 const fadeIn = () => {
 	gsap.to(DOM.imageWrapper, {
-		autoAlpha: 0.34,
+		autoAlpha: 0.4,
 		y: '-30%',
 		ease: 'power1.out',
 		scrollTrigger: {
