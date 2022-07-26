1f17b0d3da8b7d3064a0c0689928f6d0ee6db953
Eduardo Bonamigo <edbmuller@gmail.com>
2022-07-26 18:58:39 -0300

    Disable magneticElement on mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,18 @@
+import mobile from 'is-mobile'
 import './checkMobileRefreshOnResize'
 
 // TODO: Trigger after imagesLoaded
-import './animations/toggle-theme'
-// import "./animations/cursor"
 import './animations/loading-intro'
+
+import './animations/toggle-theme'
 import './animations/header-menu'
 import './animations/projects'
-import './animations/magneticElement'
+
+import magneticElement from './animations/magneticElement'
+
+if (!mobile({ tablet: true })) {
+	magneticElement()
+}
 
 // TODO: ativar algum experimento com webgl quando usar o macete
 console.log('Macete: → ↑ ← ↓ e d !')
