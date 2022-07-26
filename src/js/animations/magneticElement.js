f08ca97f690c364c481187b1da264819f11f36b2
Eduardo Bonamigo <edbmuller@gmail.com>
2022-07-26 18:57:42 -0300

    Disable magneticElement on mobile

--- src/js/animations/magneticElement.js
+++ src/js/animations/magneticElement.js
@@ -5,41 +5,49 @@ const DOM = {
 	areaAroundTarget: document.querySelector('.section.contact')
 }
 
-const targetOffsetTop = DOM.target.offsetTop
-const targetHeight = DOM.target.clientHeight
-const targetOffsetLeft = DOM.target.offsetLeft
-const targetWidth = DOM.target.clientWidth
+export default function magneticElement() {
+	const targetOffsetTop = DOM.target.offsetTop
+	const targetHeight = DOM.target.clientHeight
+	const targetOffsetLeft = DOM.target.offsetLeft
+	const targetWidth = DOM.target.clientWidth
 
-const mouseDistanceFromElementY = (mouseY) =>
-	mouseY - (targetOffsetTop + targetHeight / 2)
+	const mouseDistanceFromElementY = (mouseY) =>
+		mouseY - (targetOffsetTop + targetHeight / 2)
 
-const mouseDistanceFromElementX = (mouseX) =>
-	mouseX - (targetOffsetLeft + targetWidth / 2)
+	const mouseDistanceFromElementX = (mouseX) =>
+		mouseX - (targetOffsetLeft + targetWidth / 2)
 
-const getPercentageOfValue = (percentage, valueNumber) =>
-	((percentage / 100) * valueNumber).toFixed(0)
+	const getPercentageOfValue = (percentage, valueNumber) =>
+		((percentage / 100) * valueNumber).toFixed(0)
 
-let started = 0
-const viewport = DOM.areaAroundTarget.clientWidth
-// Make an element follow the mouse, based on element position
-DOM.areaAroundTarget.addEventListener('mousemove', (e) => {
-	const distanceY = getPercentageOfValue(10, mouseDistanceFromElementY(e.pageY))
-	const distanceX = getPercentageOfValue(10, mouseDistanceFromElementX(e.pageX))
+	let started = 0
+	const viewport = DOM.areaAroundTarget.clientWidth
+	// Make an element follow the mouse, based on element position
+	DOM.areaAroundTarget.addEventListener('mousemove', (e) => {
+		const distanceY = getPercentageOfValue(
+			10,
+			mouseDistanceFromElementY(e.pageY)
+		)
+		const distanceX = getPercentageOfValue(
+			10,
+			mouseDistanceFromElementX(e.pageX)
+		)
 
-	if (started === 0 && viewport > 1024) {
-		gsap.to(DOM.target, { autoAlpha: 1, duration: 0.6 })
-		started = 1
-	}
+		if (started === 0 && viewport > 1024) {
+			gsap.to(DOM.target, { autoAlpha: 1, duration: 0.6 })
+			started = 1
+		}
 
-	gsap.to(DOM.target, {
-		x: `${distanceX}px`,
-		duration: 1,
-		ease: 'power2.out'
+		gsap.to(DOM.target, {
+			x: `${distanceX}px`,
+			duration: 1,
+			ease: 'power2.out'
+		})
+
+		gsap.to(DOM.target, {
+			y: `${distanceY}px`,
+			duration: 1,
+			ease: 'power2.out'
+		})
 	})
-
-	gsap.to(DOM.target, {
-		y: `${distanceY}px`,
-		duration: 1,
-		ease: 'power2.out'
-	})
-})
+}
